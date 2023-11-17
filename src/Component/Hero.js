bf22e753e0242c11ecb49e9f6dfa5c80bac568ef
Muhammad Hassan Memon <134962710+Hassan-memon009@users.noreply.github.com>
2023-11-17 15:15:09 +0500

    m

---
+++ src/Component/Hero.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import '../Css/Global.css'
+
+function Welcome() {
+  return (
+    <div className='hero-section'>
+      <p className='indulge-in-a'>
+        Indulge in a symphony of flavors at our artisanal bakery. <br />
+        Welcome to a world where the aroma of freshly baked <br />
+        wonders captivates your senses. Our master bakers <br />
+        craft each creation with passion, using only the finest <br />
+        ingredients to ensure every bite is pure delight. <br />
+        <br />
+        From flaky croissants to decadent cakes, experience <br />
+        the epitome of taste and craftsmanship. Whether it&#39;s <br />a special
+        occasion or a craving for the extraordinary, <br />
+        our bakery offers a haven for your palate. Elevate <br />
+        your moments with the magic of our oven-fresh delights. <br />
+        Your journey to exceptional taste begins here.
+      </p>
+      <img
+        className='image'
+        alt='Image'
+        src='https://cdn.animaapp.com/projects/655733e9ec0d332ecbfafeba/releases/65573522f1a1f77db0a79f9a/img/image-2.png'
+      />
+    </div>
+  )
+}
+
+export default Welcome
